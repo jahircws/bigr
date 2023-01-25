f32c0fffc602916071f4b28f08a22c9d3cc83d3b
Jahir Tarafder <jahir@cwsinfotech.com>
2023-01-25 16:24:54 +0530

    ui changed

--- assets/js/dapi.js
+++ assets/js/dapi.js
@@ -11,6 +11,8 @@ var handler = Dapi.create({
     clientHeaders: {},
     clientBody: {},
     onSuccessfulLogin: function(bankAccount) {
+        $('#dapiOff').addClass('text-success').html('<i class="fa fa-check"></i> Integration Done').show();
+        $('#dapi-auth').html('<span class="spinner-border spinner-border-sm text-info"></span> We are authenticating').show();
         $('#dapiOn').hide();
         ba = bankAccount;
         console.log(ba)
@@ -20,15 +22,16 @@ var handler = Dapi.create({
             "Your account details",
             payload.accounts,
             (account) => {
-                $('#ClientLogged').show();
+                $('#dapi-auth').addClass('text-success').html('<i class="fa fa-check"></i> Authentication Done');
                 accountID = account.id;
-                console.dir(account)
+                // console.dir(account)
+                getIntervalTransactions();
             },
             () => {
                 console.dir("User Cancelled")
             })
         })
-        // getIntervalTransactions();
+        
     },
     onFailedLogin: function(err) {
         if (err != null) {
@@ -40,7 +43,8 @@ var handler = Dapi.create({
     },
     onReady: function() {
         $('#dapiOn').removeAttr('disabled');
-        alert('Login is ready')
+        $('#dapi-loading').hide();
+        // alert('Login is ready')
         // handler.open(); // opens the Connect widget
     },
     onExit: function() {
@@ -57,43 +61,45 @@ var handler = Dapi.create({
 $('#dapiOn').on('click', () => {
     handler.open();
 });
-$('#dapiOff').on('click', () => {
-    window.location.reload();
-    // ba = null;
-    // handler.open();
-})
+// $('#dapiOff').on('click', () => {
+//     window.location.reload();
+//     // ba = null;
+//     // handler.open();
+// })
 
 function getIntervalTransactions(start_date=null, end_date=null){
     if(ba){
+        $('#dapi-transaction').html('<span class="spinner-border spinner-border-sm text-info"></span> We are analyzing your transactions').show();
         if(start_date === null && end_date === null){
             end_date = moment();
             start_date = moment().startOf('month').subtract(5, 'months');
         }
-        console.log(start_date.format('YYYY-MM-DD'), end_date)
         ba.data.getTransactions(accountID, start_date.format('YYYY-MM-DD'), end_date.format('YYYY-MM-DD')).then(transactionsResponse => {
             if(transactionsResponse.status === "done") {
-                let transHtml = '';
+                let transHtml = [];
                 console.dir(transactionsResponse.transactions)
                 transactionsResponse.transactions.map((item, i) => {
-                    transHtml += '<tr>\
-                            <td>'+(i+1)+'</td>\
-                            <td>'+moment(item.date).format('DD-MM-YYYY hh:mm a')+'</td>\
-                            <td>'+item.description+'<br>'+item.details+'</td>\
-                            <td>'+item.reference+'</td>\
-                            <td>'+(item.type).toUpperCase()+'</td>\
-                            <td>'+Number(item.amount).toLocaleString('en-AE', {style: 'currency', currency: item.currency.code})+'</td>\
-                            </tr>'
+                    transHtml.push({
+                        id: (i+1),
+                        transaction_date: moment(item.date).format('YYYY-MM-DD HH:mm:ss'),
+                        details: item.description+'<br>'+item.details,
+                        type: (item.type).toUpperCase(),
+                        reference: item.reference,
+                        amount: item.amount,
+                    });
                 })
-                $('#transData').html(transHtml);
+                console.log(transHtml)
             } else {
                 console.error("API Responded with an error")
                 console.dir(transactionsResponse)
             }
+            $('#dapi-transaction').addClass('text-success').html('<i class="fa fa-check"></i>Transactions Analysis Done');
         })
         .catch(error => {
             console.dir(error)
+            $('#dapi-transaction').html('<i class="fa fa-exclamation-circle"></i>Transactions Analysis Failed');
         })
     }else{
-        alert('First login in to your Dapi account')
+        alert('First login in to your bank account')
     }
 }
